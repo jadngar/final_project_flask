4261a6376ba074bea8a24c6c2464a70351e86b0d
jadngar <juanadn89@gmail.com>
2026-09-12 22:14:27 -0400

    First commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,4 +7,24 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    return {
+           'anger': anger_score,
+           'disgust': disgust_score,
+           'fear': fear_score,
+           'joy': joy_score,
+           'sadness': sadness_score,
+           'dominant_emotion': dominant_emotion
+           }
+ 
